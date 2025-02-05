e38f11827f6668bf589e6454f1057fd6433b5cb2
Anthony Barahona <anthony@pencilz.io>
2025-02-05 15:46:21 -0500

    fix: certain updates

--- packages/storage/previews/index.js
+++ packages/storage/previews/index.js
@@ -1,49 +1,47 @@
-const AWS = require('aws-sdk');
-const SPACES_ENDPOINT = process.env['SPACES_ENDPOINT']; 
-const SPACES_NAME = process.env['SPACES_NAME'];
-const SPACES_KEY = process.env['SPACES_KEY']
-const SPACES_SECRET = process.env['SPACES_SECRET']
+const AWS = require("aws-sdk");
+const SPACES_ENDPOINT = process.env["SPACES_ENDPOINT"];
+const SPACES_NAME = process.env["SPACES_NAME"];
+const SPACES_KEY = process.env["SPACES_KEY"];
+const SPACES_SECRET = process.env["SPACES_SECRET"];
 
 const s3 = new AWS.S3({
   endpoint: SPACES_ENDPOINT,
   accessKeyId: SPACES_KEY,
-  secretAccessKey: SPACES_SECRET
-})
+  secretAccessKey: SPACES_SECRET,
+});
 
 async function main(args) {
   if (args.__ow_method == "options") {
     return {
       headers: {
-        'Access-Control-Allow-Methods': 'OPTIONS, POST',
-        'Access-Control-Allow-Origin': '*'
+        "Access-Control-Allow-Methods": "OPTIONS, POST",
+        "Access-Control-Allow-Origin": "*",
       },
-      statusCode: 200
-    }
+      statusCode: 200,
+    };
   }
-  
+
   const fileName = args.file_name;
   const contentType = args.content_type;
 
   if (!fileName || !contentType) {
-    console.log('missing file_name or content_type')
+    console.log("missing file_name or content_type");
     return {
       statusCode: 400,
       body: {
-        message: 'missing file_name or content_type',
-      }
-    }
+        message: "missing file_name or content_type",
+      },
+    };
   }
 
   const params = {
     Bucket: SPACES_NAME,
     Fields: {
-      'Content-Type': contentType,
+      "Content-Type": contentType,
       key: fileName,
     },
     Expires: 300,
-    Conditions: [
-      { 'acl': 'public-read' }
-    ]
+    Conditions: [{ acl: "public-read" }],
   };
 
   try {
@@ -51,29 +49,28 @@ async function main(args) {
       s3.createPresignedPost(params, (err, data) => {
         if (err) {
           reject(err);
-          console.error(data)
+          console.error(data);
           return;
         }
         resolve(data);
-      })
-    })
-    console.log(`Successfully signed payload for ${signedPayload.url}/${signedPayload.fields.key}`)
+      });
+    });
+    console.log(`Successfully signed payload for ${signedPayload.url}/${signedPayload.fields.key}`);
 
     return {
       statusCode: 200,
       body: {
         payload: signedPayload,
-      }
-    }
+      },
+    };
   } catch (error) {
     return {
       statusCode: 400,
       body: {
         message: `unable to get signed url: ${error.message}`,
-      }
-    }
+      },
+    };
   }
 }
 
 exports.main = main;
-
